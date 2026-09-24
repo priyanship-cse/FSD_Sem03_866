c22e03962888af6d5be25afe874007e28a2bc40d
priyanshi_cse <priyanshipanwar348@gmail.com>
2026-09-24 11:23:11 +0530

    add get code

---
+++ getin.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let students = [
+    { id: 1, name: "Rahul", branch: "CSE" },
+    { id: 2, name: "Aman", branch: "IT" }
+];
+
+// GET request
+app.get('/', (req, res) => {
+    res.send("Server is running");
+});
+
+// POST request
+app.post('/students', (req, res) => {
+    const newStudent = req.body;
+
+    students.push(newStudent);
+
+    res.status(201).json({
+        message: "Student added successfully",
+        student: newStudent
+    });
+});
+
+// Start server
+app.listen(3005, () => {
+    console.log("Server running at port 3005");
+});
